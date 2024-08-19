afb992eb60c9ee0554095d3323a934b15b9beb7a
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-19 15:56:08 +0300

    double checking

--- Engine-Search/src/App.java
+++ Engine-Search/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("does it work?");
+        System.out.println("does it work twice??");
     }
 }
