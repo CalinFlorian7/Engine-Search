05eee385974298eb8ef851e23800129ec1f2dc10
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-20 20:14:13 +0300

    getter added and method created to populate the registry

--- Engine-Search/src/entry/EntryRegistry.java
+++ Engine-Search/src/entry/EntryRegistry.java
@@ -1,4 +1,6 @@
 package entry;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.*;
 public class EntryRegistry {
     private Set<Entry>entryRegistry;
@@ -6,9 +8,39 @@ public class EntryRegistry {
    public EntryRegistry() {
 entryRegistry=new HashSet<Entry>();    
 }
-   
-  private void addEntry(Entry entry) {
-         entryRegistry.add(entry);
+   public Set<Entry> getEntryRegistry() {
+    return entryRegistry;
+   }
+  
+    public void uploadEntriesFromFile(String filePath)
+    {
+     
+      try{
+        BufferedReader bufferedReader=new BufferedReader(new FileReader(filePath));
+        String line;
+        while((line=bufferedReader.readLine())!=null)
+        {
+          Entry entry=new Entry(line.trim());
+          entryRegistry.add(entry);
+        }
+bufferedReader.close();
+
+
+      }
+      catch(Exception e)
+      {
+        System.out.println("Error in uploading entries from file");
+          e.printStackTrace();
+      }
+      finally
+      {
+          System.out.println("Entries uploaded successfully");
+      }
+      
+      
+
+
+
     }
     
 }
