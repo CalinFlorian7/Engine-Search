b1acbca4b1074a72de33d8f83ca6b2781afba482
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-20 20:07:10 +0300

    testing the algorithm

--- Engine-Search/src/App.java
+++ Engine-Search/src/App.java
@@ -1,5 +1,29 @@
+import entry.EntryRegistry;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+import engine.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("does it work twice??");
+      
+        EntryRegistry entryRegistry = new EntryRegistry();
+        
+        entryRegistry.uploadEntriesFromFile("./data/entries.txt");
+        Engine engine = new Engine();
+         BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+        System.out.println("Enter the query:\n");
+        String query=reader.readLine();
+
+
+
+
+        engine.getEngineSearchResult(query, entryRegistry);
+        reader.close();
+       
+
     }
 }
