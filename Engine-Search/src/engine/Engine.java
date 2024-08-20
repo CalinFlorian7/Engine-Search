a0fc681c258b3d2ae89f24b9df5acb7b2bff0cc0
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-20 14:08:37 +0300

    Engine class created with specific attributes fo the algorithm

---
+++ Engine-Search/src/engine/Engine.java
@@ -0,0 +1,52 @@
+package engine;
+
+import java.util.List;
+
+public class Engine {
+    
+    private List<String> words;
+    private boolean typo;
+    private int proximity;
+    private List<String> attributes;
+    private String sort;
+    private double exactness;
+    protected Engine(){
+        this.words = null;
+        this.typo = false;
+        this.proximity = 0;
+        this.attributes = null;
+        this.sort = null;
+        this.exactness = 0;
+    }
+  protected void setWords(List<String>words)
+  {
+        this.words=words;
+  }
+    protected void setTypo(boolean typo)
+    {
+            this.typo=typo;
+    }
+    protected void setProximity(int proximity)
+    {
+            this.proximity=proximity;
+    }
+    protected void setAttributes(List<String>attributes)
+    {
+            this.attributes=attributes;
+    }
+    protected void setSort(String sort)
+    {
+            this.sort=sort;
+    }
+    protected void setExactness(double exactness)
+    {
+            this.exactness=exactness;
+    }
+
+
+
+
+
+
+    
+}
