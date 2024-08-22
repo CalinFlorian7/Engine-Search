6740f48a9af104295afaa04775c81f7d11cafb75
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-22 13:14:54 +0300

    case added for the factory class

--- Engine-Search/src/result/ResultFactory.java
+++ Engine-Search/src/result/ResultFactory.java
@@ -1,14 +1,16 @@
 package result;
 
 public class ResultFactory {
-    public static Result createResult(ResultTypes type, String title)
+    public static Result createResult(ResultTypes type, String title,double levenstheinWeight,double treshold)
     {
         switch(type)
         {
             case LEVENSTHEIN_RESULT:
                 return new LevenstheinResult(title);
-            // case RELEVANT_RESULT:
-            //     return new RelevantResult(title);
+            case RELEVANCE_RESULT:
+                return new RelevanceResult(title,levenstheinWeight);
+                case RELEVANCE_TRESHOLD_RESULT:
+                return new RelevanceTresholdResult(title,levenstheinWeight,treshold);
             default:
                 return null;
         }
